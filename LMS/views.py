a8b45270a7547a6ab815a1e7d4df8cf345da462a
runalb <65166862+runalb@users.noreply.github.com>
2022-09-15 18:42:09 +0530

    LMS project basic done

--- LMS/views.py
+++ LMS/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("INDEX PAGE")
-    #return render(request,'index.html',context={'title':'Library Management System'})
+    return render(request,'index.html',context={'title':'Library Management System'})
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,7 +4,11 @@ from . import views
 app_name = 'dashboard'
 
 urlpatterns = [
-    #path('',views.dashboard_index, name='dashboard_index'),
-   
-    #path('users/', views.users, name='users'),
+    path('',views.dashboard_index, name='dashboard_index'),
+    path('add-products/', views.add_products, name='add_products'),
+    path('search-products/', views.search_available_products, name='search_available_products'),
+    path('view-available-products/', views.view_available_products, name='view_available_products'),
+    path('sell-available-products/', views.sell_available_products, name='sell_available_products'),
+    path('view-sold-products/', views.view_sold_products, name='view_sold_products'),
+    path('users/', views.users, name='users'),
 ]
